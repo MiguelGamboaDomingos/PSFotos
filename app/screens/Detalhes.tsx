97ea49d85c128b051c0280ff24dc40acd7e4ae8a
Miguelluis <domingosm412@gmail.com>
2023-11-13 00:07:07 +0100

    Detalhes do projecto

--- app/screens/Detalhes.tsx
+++ app/screens/Detalhes.tsx
@@ -1,3 +1,20 @@
+/**
+ * Desenvolvedores:
+ *  - Miguel Gamboa (Número de Matrícula: 20170757, Email: domingosm412@gmail.com)
+ *  - Diangana Patriarca Ferraz Fortuna (Número de Matrícula: 20190442, Email: dianganaf12@gmail.com)
+ *  - Marcelo Bastos (Número de Matrícula: 20181641, Email: marcelo.atkins@gmail.com)
+ * 
+ * Professor: João Costa
+ * Cadeira: Aplicações Móveis
+ * Ano Letivo: 2023/24
+ * Curso: Engenharia Informática
+ * Turma: EIN7-T1
+ * 
+ * Descrição:
+ * Esta aplicação móvel tem como objetivo permitir aos utilizadores criar álbuns de fotos
+ * sincronizando suas contas em provedores de armazenamento em nuvem, como Dropbox e Google Drive.
+ * Além disso, oferece recursos para adicionar utilizadores, criar álbuns e outras funcionalidades.
+ */
 import { View, Text } from 'react-native'
 import React from 'react'
 
--- app/screens/InsideLayout.tsx
+++ app/screens/InsideLayout.tsx
@@ -1,4 +1,21 @@
-// InsideLayout.tsx
+/**
+ * Desenvolvedores:
+ *  - Miguel Gamboa (Número de Matrícula: 20170757, Email: domingosm412@gmail.com)
+ *  - Diangana Patriarca Ferraz Fortuna (Número de Matrícula: 20190442, Email: dianganaf12@gmail.com)
+ *  - Marcelo Bastos (Número de Matrícula: 20181641, Email: marcelo.atkins@gmail.com)
+ * 
+ * Professor: João Costa
+ * Cadeira: Aplicações Móveis
+ * Ano Letivo: 2023/24
+ * Curso: Engenharia Informática
+ * Turma: EIN7-T1
+ * 
+ * Descrição:
+ * Esta aplicação móvel tem como objetivo permitir aos utilizadores criar álbuns de fotos
+ * sincronizando suas contas em provedores de armazenamento em nuvem, como Dropbox e Google Drive.
+ * Além disso, oferece recursos para adicionar utilizadores, criar álbuns e outras funcionalidades.
+ */
+
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { FIREBASE_AUTH } from '../../firebaseConfig';
